22cb5233e69c6d9c2c0ba1f0419bfcca1e2300e6
Darryl Balderas <Darryl.Balderas@ibm.com>
2017-12-06 09:02:10 -0800

    Fix travis errors

--- containers/front-end/public/js/app.js
+++ containers/front-end/public/js/app.js
@@ -1,36 +1,35 @@
-'use strict';
+"use strict";
 // Declare app level module which depends on filters, and services
-// var angular = require('angular');
 
-var app = angular.module('dashBoardApp', []);
+var app = angular.module("dashBoardApp", []);
   
 app.controller("mapCtrl", function($scope, $http){
   $http.get("http://169.46.74.117/get_events")
     .then(function(res) {
-      $scope.map = res.data[0].map    
+      $scope.map = res.data[0].map;
       $scope.beacons = res.data[0].beacons;
-  });
+    });
 });
 
 app.controller("dashboardCtrl", function($scope, $http){
   $http.get("http://169.46.74.117/get_events")
     .then(function(res) {
-      $scope.conferenceTitle = res.data[0].eventDescription + " " + res.data[0].location + " Conference";
-      $scope.calories = 10000;
-      $scope.steps = 23000;
-      $scope.participants = 240;
-      $scope.totalDistance = 52;
-      $scope.fitCoins = 750;
-  });
+        $scope.conferenceTitle = res.data[0].eventDescription + " " + res.data[0].location + " Conference";
+        $scope.calories = 10000;
+        $scope.steps = 23000;
+        $scope.participants = 240;
+        $scope.totalDistance = 52;
+        $scope.fitCoins = 750;
+    });
 });
 
-angular.forEach(['x', 'y', 'width', 'height'], function(name) {
-  var ngName = 'ng' + name[0].toUpperCase() + name.slice(1);
+angular.forEach(["x", "y", "width", "height"], function(name) {
+  var ngName = "ng" + name[0].toUpperCase() + name.slice(1);
   app.directive(ngName, function() {
     return function(scope, element, attrs) {
       attrs.$observe(ngName, function(value) {
         attrs.$set(name, value*10); 
-      })
+      });
     };
   });
 });
