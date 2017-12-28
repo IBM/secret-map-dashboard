cc8492ed60d44eebfad4551a7b040d8522bb1c15
Darryl Balderas <Darryl.Balderas@ibm.com>
2017-12-28 16:52:53 -0600

    Fix eslint errors

--- containers/front-end/protractor.conf.js
+++ containers/front-end/protractor.conf.js
@@ -1,16 +1,11 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-<<<<<<< HEAD
 const { SpecReporter } = require("jasmine-spec-reporter");
-=======
-const { SpecReporter } = require('jasmine-spec-reporter');
->>>>>>> 57a0fb364bb841909c0ac2330f374eb37d2780ca
 
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-<<<<<<< HEAD
     "./e2e/**/*.e2e-spec.ts"
   ],
   capabilities: {
@@ -19,29 +14,14 @@ exports.config = {
   directConnect: true,
   baseUrl: "http://localhost:4200/",
   framework: "jasmine",
-=======
-    './e2e/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
->>>>>>> 57a0fb364bb841909c0ac2330f374eb37d2780ca
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
     print: function() {}
   },
   onPrepare() {
-<<<<<<< HEAD
     require("ts-node").register({
       project: "e2e/tsconfig.e2e.json"
-=======
-    require('ts-node').register({
-      project: 'e2e/tsconfig.e2e.json'
->>>>>>> 57a0fb364bb841909c0ac2330f374eb37d2780ca
     });
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
