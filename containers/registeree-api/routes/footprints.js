5515ba831067a14e47b6777ee8a0e0920215c833
Darryl Balderas <Darryl.Balderas@ibm.com>
2018-02-22 17:14:48 -0800

    Initial registeree mongo database
    Created registeree and footprint mongose schema
    Implemented node server to host mongodb endpoints
    
    Related-issues: #119
    Signed-off by: Darryl Balderas <drrylbalderas@gmail.com>

---
+++ containers/registeree-api/routes/footprints.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+const Footprints = require("../models/footprint");
+
+// endpoints for booth
+router.get("/", function(req, res) {
+  res.send("Footprints");
+});
+
+router.get("/:footprintId", function(req, res) {
+  res.send("Footprint");
+});
+
+router.post("/add", function(req, res) {
+  // JSON in req.body
+  // Insert input validation
+  let addFootprint = new Footprints(req.body);
+  res.send("Saved Footprint.");
+});
+
+router.post("/remove/:footprintId", function(req, res) {
+    // JSON in req.body
+    // Insert input validation
+    let removeFootprint = new Footprints(req.body);
+    res.send("Removed Footprint.");
+});
+
+module.exports = router;
---
+++ containers/registeree-api/test/footprintTest.js
@@ -0,0 +1,39 @@
+process.env.UNIT_TEST = "test";
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const should = chai.should();
+
+const Footprint = require('../models/registeree');
+
+chai.use(chaiHttp);
+//Our parent block
+describe('Footprints', () => {
+  beforeEach((done) => { //Before each test we empty the database
+    done();
+  });
+  /*
+  * Test the /GET route
+  */
+  describe('GET /footprints', () => {
+    it('it should GET all the footprints', (done) => {
+      done();
+    });
+  });
+  /*
+  * Test the /POST route
+  */
+  describe('POST /footprints/add', () => {
+    it('it should POST add a footprint', (done) => {
+      done();
+    });
+  });
+
+  describe('POST /footprints/remove/:footprintId', () => {
+    it('it should POST remove a print', (done) => {
+      done();
+    });
+  });
+
+});
